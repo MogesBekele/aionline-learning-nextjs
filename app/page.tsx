1f7c1efb550da241c3667b00daff5629648d6614
MogesBekele <mogesbekele32@gmail.com>
2025-06-13 14:49:40 +0300

    coplete the autentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 const page = () => {
   return (
     <div className='text-center'>
       <h1>this the home page</h1>
       <Button>add to card</Button>
+      <UserButton/>
     </div>
   )
 }

8d31f5bf7ad68c0cda3883d366cd3713cfbce0d3
MogesBekele <mogesbekele32@gmail.com>
2025-06-13 17:42:38 +0300

    apply tailwind on main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from '@clerk/nextjs'
 
 const page = () => {
   return (
-    <div className='text-center'>
+    <div className='text-center flex items-center justify-center'>
       <h1>this the home page</h1>
       <Button>add to card</Button>
       <UserButton/>

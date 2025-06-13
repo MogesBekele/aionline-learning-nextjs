25be2ccee03eeca65593b0de5068d7332788d4f1
MogesBekele <mogesbekele32@gmail.com>
2025-06-13 14:08:51 +0300

    import the button from shadecn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
     <div className='text-center'>
       <h1>this the home page</h1>
+      <Button>add to card</Button>
     </div>
   )
 }

3ba6f949cbe86a4979d626d06e999645e35d6fcf
MogesBekele <mogesbekele32@gmail.com>
2025-06-16 15:49:14 +0300

    add the sidebar comtent

--- app/workspace/provider.tsx
+++ app/workspace/provider.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSidebar from "./_components/AppSidebar";
 
 const WorkspaceProvider = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider>
+    <AppSidebar/>
+      <SidebarTrigger />
+      <div>{children}</div>
+    </SidebarProvider>
+  );
 };
 
 export default WorkspaceProvider;

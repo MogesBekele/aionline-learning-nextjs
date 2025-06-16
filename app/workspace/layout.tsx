3a801f613d097753734b3171027a978306de60e9
MogesBekele <mogesbekele32@gmail.com>
2025-06-16 15:11:16 +0300

    create the workspace layout

---
+++ app/workspace/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WorkspaceLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>workspace layout</div>;
+};
+
+export default WorkspaceLayout;

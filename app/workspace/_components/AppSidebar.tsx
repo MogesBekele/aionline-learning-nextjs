6efb08150dae61315f40946029bd83f88360f21b
MogesBekele <mogesbekele32@gmail.com>
2025-06-17 15:43:12 +0300

    add the items option

--- app/workspace/_components/AppSidebar.tsx
+++ app/workspace/_components/AppSidebar.tsx
@@ -1,21 +1,92 @@
 import React from "react";
-import Image from "next/image"; 
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+
+import {
+  Book,
+  Compass,
+  LayoutDashboard,
+  PencilRulerIcon,
+  UserCircle2Icon,
+} from "lucide-react";
+import Link from "next/link";
+
+const SidebarOptions = [
+  {
+    title: "Dashboard",
+    icon: LayoutDashboard,
+    path: "/#",
+  },
+  {
+    title: "My Learning",
+    icon: Book,
+    path: "/workspace/my-courses",
+  },
+  {
+    title: "Explore Courses",
+    icon: Compass,
+    path: "/workspace/explore",
+  },
+  {
+    title: "AI Tools",
+    icon: PencilRulerIcon,
+    path: "/workspace/ai-tools",
+  },
+  {
+    title: "Billing",
+    icon: LayoutDashboard,
+    path: "/workspace/billing",
+  },
+  {
+    title: "Profile",
+    icon: UserCircle2Icon,
+    path: "/workspace/billing",
+  },
+];
 function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader className="p-4">
-    <Image className="text-black"  alt="logo" src="/logo.svg" width={40} height={40} />
+        <Image
+          className="text-black"
+          alt="logo"
+          src="/logo.svg"
+          width={40}
+          height={40}
+        />
       </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <Button>Create New Courese</Button>
+        </SidebarGroup>
+
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {SidebarOptions.map((items, index) => (
+                <SidebarMenuItem key={index}>
+                  <SidebarMenuButton asChild>
+                    <Link href={items.path}>
+                      <items.icon className="mr-2 h-4 w-4" />
+                      <span>{items.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>

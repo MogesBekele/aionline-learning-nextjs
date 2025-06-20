e80ecff27676e569a25c294515cadb5ba05bc002
MogesBekele <mogesbekele32@gmail.com>
2025-06-20 17:10:27 +0300

    add all the nesseccary content on courselist

--- app/workspace/_components/CourseList.tsx
+++ app/workspace/_components/CourseList.tsx
@@ -1,15 +1,17 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 function CourseList() {
   const [CourseList, setCourseList] = useState([]);
   return (
-    <div>
+    <div className="mt-10">
       <h2 className="font-bold text-3xl">Courses List</h2>
       {CourseList?.length == 0 ? (
-        <div>
+        <div className="flex items-center flex-col justify-center p-7 border rounded-xl mt-2 bg-secondary">
          <Image src={'/image.png'} alt= 'image' width={100} height={100}/>
+         <h2 className="text-xl my-2 font-bold">look like you don't have any course</h2>
+         <Button>create your first course</Button>
         </div>
       ) : (
         <div>list of the courses</div>

2cb551c494adb9058440274d6622eac52f3ccae8
MogesBekele <mogesbekele32@gmail.com>
2025-06-20 16:21:15 +0300

    appheader section

--- app/workspace/_components/AppHeader.tsx
+++ app/workspace/_components/AppHeader.tsx
@@ -1,8 +1,13 @@
+import { SidebarTrigger } from '@/components/ui/sidebar'
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function AppHeader() {
   return (
-    <div>AppHeader</div>
+    <div className='p-4 justify-between items-center flex shadow-sm'>
+      <SidebarTrigger/>
+      <UserButton/>
+    </div>
   )
 }
 

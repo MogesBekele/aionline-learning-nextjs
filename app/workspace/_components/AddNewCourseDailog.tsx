565e89599c4bc82aedc2a32eb26c959e2e83d03a
MogesBekele <mogesbekele32@gmail.com>
2025-06-21 17:05:44 +0300

    dailog components

--- app/workspace/_components/AddNewCourseDailog.tsx
+++ app/workspace/_components/AddNewCourseDailog.tsx
@@ -7,16 +7,27 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 function AddNewCourseDailog({children}:{children:React.ReactNode}) {
   return (
    <Dialog>
   <DialogTrigger asChild>{children}</DialogTrigger>
   <DialogContent>
     <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
+      <DialogTitle>Create New Course Using AI</DialogTitle>
+      <DialogDescription asChild>
+      <div>
+        <div>
+          <label>Course Name</label>
+          <Input placeholder='Course Name'/>
+        </div>
+         <div>
+          <label>Course Description (Optional)</label>
+          <Textarea placeholder='Course Description'/>
+        </div>
+
+      </div>
       </DialogDescription>
     </DialogHeader>
   </DialogContent>

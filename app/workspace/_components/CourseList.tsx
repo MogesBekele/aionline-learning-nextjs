1b6f23020259bf84006d21e0bc8178415aa78f9c
MogesBekele <mogesbekele32@gmail.com>
2025-06-21 17:05:40 +0300

    add description in dailog

--- app/workspace/_components/CourseList.tsx
+++ app/workspace/_components/CourseList.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import AddNewCourseDailog from "./AddNewCourseDailog";
 function CourseList() {
   const [CourseList, setCourseList] = useState([]);
   return (
@@ -9,9 +10,13 @@ function CourseList() {
       <h2 className="font-bold text-3xl">Courses List</h2>
       {CourseList?.length == 0 ? (
         <div className="flex items-center flex-col justify-center p-7 border rounded-xl mt-2 bg-secondary">
-         <Image src={'/image.png'} alt= 'image' width={100} height={100}/>
-         <h2 className="text-xl my-2 font-bold">look like you don't have any course</h2>
-         <Button>create your first course</Button>
+          <Image src={"/image.png"} alt="image" width={100} height={100} />
+          <h2 className="text-xl my-2 font-bold">
+            look like you don't have any course
+          </h2>
+          <AddNewCourseDailog>
+            <Button>create your first course</Button>
+          </AddNewCourseDailog>
         </div>
       ) : (
         <div>list of the courses</div>

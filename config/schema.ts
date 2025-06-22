a4c720dc1a6fb70ea288127cf440afa47305ab8f
MogesBekele <mogesbekele32@gmail.com>
2025-06-22 17:40:56 +0300

    create the schema

--- config/schema.ts
+++ config/schema.ts
@@ -1,9 +1,20 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
+import { integer, boolean, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  subscriptionId: varchar()
+  subscriptionId: varchar(),
+});
+
+export const coursesTable = pgTable("courses", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  cid: varchar().notNull(),
+  description: varchar(),
+  name: varchar(),
+  noOfChapters: integer(),
+  includeVideo: boolean(),
+  level: varchar(),
+  category: varchar(),
+  userEmail: varchar("userEmail").references(() => usersTable.email),
 });
